746f5cf4e63a61ccf08e39235522eed596f4df1e
Yuri Mikushov <mikushov.yu@gmail.com>
2021-01-03 12:33:19 +0300

    refactor: rewrote calc component with useReducer

--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useReducer, useEffect } from 'react'
 import { MATH_OPERATORS, SERVISE_OPERATORS, ERROR_RESULT } from './../../constants'
 import {
   isNumber,
@@ -12,9 +12,74 @@ import './index.css'
 import Screen from './../Screen'
 import NumPad from './../NumPad'
 
+const CALC_ACTION_TYPE = {
+  SHOW_ENTERED_VALUE: 'ShowEnteredValue',
+  SHOW_RESULT: 'ShowResult',
+  DELETE_LAST_ENTERED_VALUE: 'DeleteLastEnteredValue',
+  CLEAR_ENTERED_VALUES: 'ClearEnteredValues',
+}
+
+function reducer(state, action) {
+  let updatedEnteredValues
+
+  switch (action.type) {
+    case CALC_ACTION_TYPE.SHOW_ENTERED_VALUE:
+      updatedEnteredValues =
+        state.enteredValues !== ERROR_RESULT
+          ? state.enteredValues.concat(action.enteredValue)
+          : '' + action.enteredValue
+
+      return {
+        previewResult: calcPreviewResult(updatedEnteredValues),
+        enteredValues: updatedEnteredValues,
+      }
+    case CALC_ACTION_TYPE.SHOW_RESULT:
+      return {
+        previewResult: '',
+        enteredValues: calcResult(state.enteredValues),
+      }
+    case CALC_ACTION_TYPE.DELETE_LAST_ENTERED_VALUE:
+      updatedEnteredValues =
+        state.enteredValues !== ERROR_RESULT
+          ? state.enteredValues.substring(0, state.enteredValues.length - 1)
+          : ''
+
+      return {
+        previewResult: calcPreviewResult(updatedEnteredValues),
+        enteredValues: updatedEnteredValues,
+      }
+    case CALC_ACTION_TYPE.CLEAR_ENTERED_VALUES:
+      return {
+        previewResult: '',
+        enteredValues: '',
+      }
+    default:
+      throw new Error()
+  }
+}
+
+function calcPreviewResult(enteredValues) {
+  const lastEnteredValue = enteredValues[enteredValues.length - 1]
+
+  return !isMathOperator(lastEnteredValue) ? calcResult(enteredValues) : ''
+}
+
+function calcResult(enteredValues) {
+  try {
+    return '' + (eval(fixEnteredValues(enteredValues)) || '')
+  } catch (e) {
+    return ERROR_RESULT
+  }
+
+  function fixEnteredValues(values) {
+    return values
+      .replaceAll(MATH_OPERATORS.MULTIPLICATION, MATH_OPERATORS.AlT_MULTIPLICATION)
+      .replaceAll(MATH_OPERATORS.DIVISION, MATH_OPERATORS.AlT_DIVISION)
+  }
+}
+
 export default function Calculator() {
-  const [previewResult, setPreviewResult] = useState('')
-  const [enteredValues, setEnteredValues] = useState('')
+  const [state, dispatch] = useReducer(reducer, { previewResult: '', enteredValues: '' })
 
   useEffect(() => {
     const keyDownHandler = (e) => keyboardInputHandler(e.key)
@@ -37,64 +102,24 @@ export default function Calculator() {
   }
 
   function showEnteredValue(enteredValue) {
-    setEnteredValues((enteredValues) => {
-      const updatedEnteredValues =
-        enteredValues !== ERROR_RESULT
-          ? enteredValues.concat(enteredValue)
-          : '' + enteredValue
-
-      setPreviewResult(calcPreviewResult(updatedEnteredValues))
-
-      return updatedEnteredValues
-    })
+    dispatch({ type: CALC_ACTION_TYPE.SHOW_ENTERED_VALUE, enteredValue })
   }
 
   function showResult() {
-    setPreviewResult('')
-    setEnteredValues((enteredValues) => calcResult(enteredValues))
+    dispatch({ type: CALC_ACTION_TYPE.SHOW_RESULT })
   }
 
   function deleteLastEnteredValue() {
-    setEnteredValues((enteredValues) => {
-      const updatedEnteredValues =
-        enteredValues !== ERROR_RESULT
-          ? enteredValues.substring(0, enteredValues.length - 1)
-          : ''
-
-      setPreviewResult(calcPreviewResult(updatedEnteredValues))
-
-      return updatedEnteredValues
-    })
+    dispatch({ type: CALC_ACTION_TYPE.DELETE_LAST_ENTERED_VALUE })
   }
 
   function clearEnteredValues() {
-    setPreviewResult('')
-    setEnteredValues('')
-  }
-
-  function calcPreviewResult(enteredValues) {
-    const lastEnteredValue = enteredValues[enteredValues.length - 1]
-
-    return !isMathOperator(lastEnteredValue) ? calcResult(enteredValues) : ''
-  }
-
-  function calcResult(enteredValues) {
-    try {
-      return '' + (eval(fixEnteredValues(enteredValues)) || '')
-    } catch (e) {
-      return ERROR_RESULT
-    }
-
-    function fixEnteredValues(values) {
-      return values
-        .replaceAll(MATH_OPERATORS.MULTIPLICATION, MATH_OPERATORS.AlT_MULTIPLICATION)
-        .replaceAll(MATH_OPERATORS.DIVISION, MATH_OPERATORS.AlT_DIVISION)
-    }
+    dispatch({ type: CALC_ACTION_TYPE.CLEAR_ENTERED_VALUES })
   }
 
   return (
     <div className='calculator'>
-      <Screen previewResult={previewResult} result={enteredValues} />
+      <Screen previewResult={state.previewResult} result={state.enteredValues} />
       <NumPad
         showEnteredValue={showEnteredValue}
         showResult={showResult}
